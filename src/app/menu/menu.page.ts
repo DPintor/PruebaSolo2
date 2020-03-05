399994a28cea53436005d8b140cf604c67119c86
DPintor <David_Pintor6@outlook.es>
2020-03-05 16:43:55 +0100

    Login + Menu dentro de la app

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController, Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
+  navigate: any;
+  ngOnInit() {}
+  constructor(private platform: Platform,
+              private splashScreen: SplashScreen,
+              private statusBar: StatusBar) {
+                    this.sideMenu();
+                    this.initializeApp();
+              }
 
-  constructor() { }
+initializeApp() {
+this.platform.ready().then(() => {
+this.statusBar.styleDefault();
+this.splashScreen.hide();
+});
+}
 
-  ngOnInit() {
-  }
-
+sideMenu() {
+this.navigate =
+[
+{
+title : 'Home',
+url   : '//menu',
+icon  : 'home'
+},
+{
+title : 'Chat',
+url   : '/menu',
+icon  : 'chatbubbles'
+},
+{
+title : 'Contacts',
+url   : '/menu',
+icon  : 'man'
+},
+{
+title : 'Exit',
+url   : '/login',
+icon  : 'exit'
+}
+];
+}
 }
